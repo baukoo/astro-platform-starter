781666f8283e2ec02d0b8f1e81e6fe30575e8d0c
Plastikaos <2odcuenta@gmail.com>
2025-06-26 09:19:56 -0500

    hola como ta

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    host: true,
+    allowedHosts: ['devserver-preview--plastikaos.netlify.app']
+  },
+  preview: {
+    host: true,
+    allowedHosts: ['devserver-preview--plastikaos.netlify.app']
+  }
+})
+    
